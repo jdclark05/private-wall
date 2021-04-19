34274a8f9d30e6823e28b723f917769ea6b115e0
James Clark <jdclark05@gmail.com>
2021-04-19 09:27:29 -0400

    clean-up

--- server.py
+++ server.py
@@ -9,7 +9,6 @@ bcrypt = Bcrypt(app)
 app.secret_key = "validation"
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
-
 @app.route('/')
 def index():
         return render_template("index.html")
